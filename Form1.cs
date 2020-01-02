fdc2dde19d971d9d3917c7293605bc1efeff4204
koland12201 <koland12201@yahoo.com.hk>
2020-01-02 17:10:12 +0800

    merging threads, checkpoint1

--- Form1.cs
+++ Form1.cs
@@ -19,6 +19,8 @@ using System.Net.Sockets;
 using System.Net;
 using System.IO;
 
+
+
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
@@ -29,8 +31,8 @@ namespace WindowsFormsApp1
         VisionaryControl control = new VisionaryControl(host);
         int index = 0;
         Bitmap bitmap_RGB;
-        
-
+        bool iswrite = false;
+        byte[] qqq;
         public Form1()
         {
             InitializeComponent();
@@ -42,8 +44,7 @@ namespace WindowsFormsApp1
             Task.Run(async () =>
             {
                 // Buffer for reading data
-                Byte[] bytes = new Byte[256];
-                String data = null;
+                Byte[] bytes = new Byte[1024];
 
                 try
                 {
@@ -51,136 +52,130 @@ namespace WindowsFormsApp1
                     TcpListener server = new TcpListener(ipAddress, 12201);
                     System.Threading.Thread.Sleep(1000);
                     server.Start();
-                    MessageBox.Show("Waiting for client to connect...");
+                    //MessageBox.Show("Waiting for client to connect...");
                     TcpClient client = server.AcceptTcpClient();
-
-
                     NetworkStream nStream = client.GetStream();
-                    
-
                     //network loop, send then wait for reply loop
+                    int i = -1;
+                    Bitmap tmp_RGB;
                     while (true)
                     {
-                        // The 'using' here will call Dispose on the client after data is sent.
-                        // This will disconnect the client
-                        //byte[] bStream = converterDemo(bitmap_RGB);
-                        try
+                        Thread.Sleep(2);
+                        i = nStream.Read(bytes, 0, bytes.Length);
+
+                        if (i > 0)
+                        {
+                            String data = null;
+                            // Translate data bytes to a ASCII string.
+                            data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                            //MessageBox.Show("Received:" + data);
+                            // Process the data sent by the client.
+                            data = data.ToUpper();
+                            //nStream.Write(bytes, 0, bytes.Length);
+
+                            if (data == "REQUEST\n")
                             {
-                            //transmit 1 frame
-                            bitmap_RGB.Save("C:/Users/Koland Mak/Desktop/UON/placement/deep learning/sample/locate box/temp.png");
-                            //nStream.Write(bStream, 0, bStream.Length);
-                            //end of stream
-                            String cmd = "END";
+                                //transmit 1 frame
+                                //compile image from 640x512x3 -> 983040x1
+                                if(!iswrite)
+                                {
+                                    byte[] bStream = ImageToByte(bitmap_RGB);
+                                    nStream.Write(bStream, 0, bStream.Length);
+                                }
+                         
+                                /*
+                                //Send back a response.
+                                String cmd = "END";
                                 Byte[] endOfStream_ary = Encoding.UTF8.GetBytes(cmd);
                                 Byte[] endOfStream = new Byte[cmd.Length + 2];
                                 for (int i2 = 0; i2 < cmd.Length; i2++)
                                 {
-                                endOfStream[i2 + 1] = endOfStream_ary[i2];
+                                    endOfStream[i2 + 1] = endOfStream_ary[i2];
                                 }
-                            endOfStream[cmd.Length + 1] = 0x0A;
-
+                                endOfStream =new Byte[1];
                                 nStream.Write(endOfStream, 0, endOfStream.Length);
-                            }
-                            catch (SocketException e1)
-                            {
-                                MessageBox.Show("SocketException: " + e1);
-                            }
+                                */
 
-                        bool Replied = false;
-                            int i;
-                            while (((i = nStream.Read(bytes, 0, bytes.Length)) != 0)&&Replied==false)
-                            {
-                                // Translate data bytes to a ASCII string.
-                                data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                                //MessageBox.Show("Received:" + data);
-
-                                // Process the data sent by the client.
-                                //data = data.ToUpper();
-
-                                //byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
-
-                                // Send back a response.
-                                //stream.Write(msg, 0, msg.Length);
                                 //MessageBox.Show("Sent" + data);
-                                Replied = true;
-
+                            }
                         }
-
-                        System.Threading.Thread.Sleep(100);
-
+                        else
+                        {
+                            int wer = 0;
+                        }
                     }
-                }
-                catch (SocketException e1)
-                {
-                    MessageBox.Show("SocketException: " + e1);
-                }   
+            }
+                catch (Exception e1)
+            {
+                MessageBox.Show("SocketException: " + e1.Message);
+            }
 
             /*
-                TcpListener server = null;
-                try
+            TcpListener server = null;
+            try
+            {
+                // Set the TcpListener on port 12201
+                Int32 port = 12201;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localAddr, port);
+
+                // Start listening for client requests.
+                server.Start();
+
+                // Buffer for reading data
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                // Enter the listening loop.
+                while (true)
                 {
-                    // Set the TcpListener on port 12201
-                    Int32 port = 12201;
-                    IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+                    MessageBox.Show("Waiting for a connection... ");
 
-                    // TcpListener server = new TcpListener(port);
-                    server = new TcpListener(localAddr, port);
+                    // Perform a blocking call to accept requests.
+                    // You could also user server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();
+                    MessageBox.Show("Connected!");
 
-                    // Start listening for client requests.
-                    server.Start();
+                    data = null;
 
-                    // Buffer for reading data
-                    Byte[] bytes = new Byte[256];
-                    String data = null;
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
 
-                    // Enter the listening loop.
-                    while (true)
+                    int i;
+
+                    // Loop to receive all the data sent by the client.
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
                     {
-                        MessageBox.Show("Waiting for a connection... ");
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        MessageBox.Show("Received:"+ data);
 
-                        // Perform a blocking call to accept requests.
-                        // You could also user server.AcceptSocket() here.
-                        TcpClient client = server.AcceptTcpClient();
-                        MessageBox.Show("Connected!");
+                        // Process the data sent by the client.
+                        data = data.ToUpper();
 
-                        data = null;
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
 
-                        // Get a stream object for reading and writing
-                        NetworkStream stream = client.GetStream();
-
-                        int i;
-
-                        // Loop to receive all the data sent by the client.
-                        while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
-                        {
-                            // Translate data bytes to a ASCII string.
-                            data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                            MessageBox.Show("Received:"+ data);
-
-                            // Process the data sent by the client.
-                            data = data.ToUpper();
-
-                            byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
-
-                            // Send back a response.
-                            stream.Write(msg, 0, msg.Length);
-                            MessageBox.Show("Sent"+ data);
-                        }
-
-                        // Shutdown and end connection
-                        client.Close();
+                        // Send back a response.
+                        stream.Write(msg, 0, msg.Length);
+                        MessageBox.Show("Sent"+ data);
                     }
+
+                    // Shutdown and end connection
+                    client.Close();
                 }
-                catch 
-                {
-                    MessageBox.Show("SocketException");
-                }
-                finally
-                {
-                    // Stop listening for new clients.
-                    server.Stop();
-                }
-                */
+            }
+            catch 
+            {
+                MessageBox.Show("SocketException");
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+            }
+            */
         });
 
 
@@ -190,13 +185,13 @@ namespace WindowsFormsApp1
                 {
                     if (!await dataStream.ConnectAsync())
                     {
-                        // Data stream connection failed
-                        MessageBox.Show("error");
+        // Data stream connection failed
+        MessageBox.Show("error");
                     }
                     if (!await control.ConnectAsync())
                     {
-                        // Data control (CoLaB) connection failed
-                    }
+        // Data control (CoLaB) connection failed
+    }
                     await control.StartAcquisitionAsync();
                 });
             }
@@ -210,30 +205,34 @@ namespace WindowsFormsApp1
             {
                 while (true)
                 {
-                    
+
                     VisionaryFrame frame = await dataStream.GetNextFrameAsync();
                     drawBox(10, 10, 100, 100);
+                   
                     //System.Threading.Thread.Sleep(1000);
                     VisionarySDepthMapData depthMap = frame.GetData<VisionarySDepthMapData>();
                     Console.WriteLine($"Frame received through step called, frame #{depthMap.FrameNumber}, timestamp: {depthMap.TimestampMs}");
-                    
+
                     //// Important: When converting multiple frames, make sure to re-use the same converter as it will result in much better performance.
                     PointCloudConverter converter = new PointCloudConverter();
                     Vector3[] pointCloud = converter.Convert(depthMap);
                     float z = pointCloud[250 * 640 + 320].Z;
                     this.label1.Text = z.ToString();
                     //MessageBox.Show("get fram");
-                   
-                 Bitmap bitmap = depthMap.ZMap.ToBitmap(8000);
 
+                    Bitmap bitmap = depthMap.ZMap.ToBitmap(8000);
                     this.pictureBox1.Image = bitmap;
 
+
+                    iswrite = true;
                     bitmap_RGB = depthMap.RgbaMap.ToBitmap();
+                    iswrite = false;
+                    //int abc = depthMap.RgbaMap.Data.Length;
                     this.pictureBox2.Image = bitmap_RGB;
 
                 }
             });
-        }   
+        }
 
         private void button_Save_Click(object sender, EventArgs e)
         {
@@ -249,32 +248,31 @@ namespace WindowsFormsApp1
             gF.DrawRectangle(Pens.Red, x, y, w, h);
 
         }
-
         private void button2_Click(object sender, EventArgs e)
         {
             drawBox(10, 10, 100, 100);
         }
-        static byte[] ImageToByte(System.Drawing.Image iImage)
-        {
-            MemoryStream mMemoryStream = new MemoryStream();
-            iImage.Save(mMemoryStream, System.Drawing.Imaging.ImageFormat.Png);
-            return mMemoryStream.ToArray();
-        }
-        public byte[] ImageToByteArray(System.Drawing.Image imageIn)
-        {
-            using (var ms = new MemoryStream())
-            {
-                imageIn.Save(ms, imageIn.RawFormat);
-                return ms.ToArray();
-            }
-        }
-        public static byte[] converterDemo(Image x)
-        {
-            ImageConverter _imageConverter = new ImageConverter();
-            byte[] xByte = (byte[])_imageConverter.ConvertTo(x, typeof(byte[]));
-            return xByte;
-        }
 
+        private byte[] ImageToByte(Bitmap img)
+        {
+            byte[] Stream=new byte[img.Width* img.Height*3];
+            int i = 0;
+            int x = 0;
+            int y = 0;
+
+            Color color;
+            for (y = 0; y < img.Height; y++)
+            {
+                for (x = 0; x < img.Width; x++)
+                {
+                    color = img.GetPixel(x, y);
+                    Stream[i++] = color.R;
+                    Stream[i++] = color.G;
+                    Stream[i++] = color.B;
+                }
+            }
+            return Stream;
+        }
 
     }
 }
